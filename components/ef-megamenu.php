a7eacb1b9ad506e1f3839694422f7bc0911ffc46
Danylo Kupriienko <79247603+dakup101@users.noreply.github.com>
2022-12-15 18:50:32 +0100

    Edits P1

--- components/ef-megamenu.php
+++ components/ef-megamenu.php
@@ -17,11 +17,11 @@
                     $link = get_term_link( $el, 'product_cat' );
                     $thumbnail_id = get_woocommerce_term_meta( $el->term_id, 'thumbnail_id', true );
                     $image = wp_get_attachment_url( $thumbnail_id );
-                    $rand = rand(1,8);
+                    $rand = rand(1, 4);
                 ?>
                 <a href="<?php echo $link; ?>"
                     class="grid text-brown hover:text-dark cursor-pointer transition-all rounded-lg">
-                    <figure class="relative text-white hover:text-brownish-light transition-all">
+                    <figure class="relative text-white hover:text-brownish-light transition-all svg-bg">
                         <figure class="absolute top-1/2 left-1/2 w-full -translate-y-1/2 -translate-x-1/2 z-10">
                             <?php switch_shape($rand); ?>
                         </figure>
--- components/ef-product-cat-slider.php
+++ components/ef-product-cat-slider.php
@@ -23,12 +23,11 @@ $catTerms = get_terms('product_cat', $args);
             $link = get_term_link( $el, 'product_cat' );
             $thumbnail_id = get_woocommerce_term_meta( $el->term_id, 'thumbnail_id', true );
             $image = wp_get_attachment_url( $thumbnail_id );
-            // $rand = rand(1,8);
-            $rand = 3;
+            $rand = rand(1, 4);
             ?>
             <a href="<?php echo $link; ?>"
                 class="grid text-brown hover:text-dark cursor-pointer transition-all rounded-lg">
-                <figure class="relative text-brownish-light hover:text-white transition-all">
+                <figure class="relative text-brownish-light hover:text-white transition-all svg-bg">
                     <figure class="absolute top-1/2 left-1/2 w-full -translate-y-1/2 -translate-x-1/2 z-10">
                         <?php switch_shape($rand); ?>
                     </figure>
