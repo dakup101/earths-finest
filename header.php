b50032c95c40d4c8620c967e1af2e9f8120e96af
Danylo Kupriienko <79247603+dakup101@users.noreply.github.com>
2022-12-17 13:34:05 +0100

    GA & GTAG

--- header.php
+++ header.php
@@ -7,10 +7,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Earth's Finest</title>
     <link rel="stylesheet" href="https://use.typekit.net/ahi5ene.css">
+    <!-- Google Tag Manager -->
+    <script>
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-58KJ6BV');
+    </script>
+    <!-- End Google Tag Manager -->
     <?php wp_head(); ?>
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58KJ6BV" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <header
         class="fixed <?php echo is_admin_bar_showing() ? 'top-8' : 'top-0' ?> top-0 left-0 w-full bg-biege-dark z-50">
         <div class="relative">
